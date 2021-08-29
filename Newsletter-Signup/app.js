25fe938e2e73ad66ab2f5df207a67b471e7906cb
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2021-08-30 02:58:08 +0530

    mailchimp fin

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const https = require('https');
 
-var app = express();
+const app = express();
 
 app.use(express.urlencoded());
 
@@ -11,6 +11,47 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/signup.html');
 })
 
+app.post('/', function(req, res){
+
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+  
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }    
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us5.api.mailchimp.com/3.0/lists/19bbabf0fd";
+
+  const options = {
+    method: "POST",
+    auth: "reuben:d091170be7eebe3510a57dd4f259a473-us5"
+  }
+
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+})
+
 app.listen(3000, () => {
   console.log('server is running on port 3000...')
 })
+
+// API Key : d091170be7eebe3510a57dd4f259a473-us5
+// Audience ID : 19bbabf0fd
