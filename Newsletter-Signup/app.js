faf01dff6c9f500288477f047536099beb5fdb97
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2021-08-30 01:09:18 +0530

    random

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -5,6 +5,12 @@ var app = express();
 
 app.use(express.urlencoded());
 
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/signup.html');
+})
+
 app.listen(3000, () => {
   console.log('server is running on port 3000...')
 })
