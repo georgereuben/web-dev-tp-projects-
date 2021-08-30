bc441da689f19c4110d3f55dd635ce8e7b5caa1a
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2021-08-31 03:30:47 +0530

    date.js init

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-
+const date = require(__dirname + '/date.js');
 const app = express();
 
 app.use(express.urlencoded());
@@ -12,24 +12,11 @@ let items = [];
 let workItems = [];
 
 app.get('/', function(req,res){
-
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options)
-
+  let day = date.getDate();
   res.render('list', {listTitle: day, newListItems: items});
-
 })
 
 app.post('/', function(req, res){
-
-  console.log(req.body.list);
 
   if(req.body.list === 'Work List'){
     workItems.push(req.body.newItem);
