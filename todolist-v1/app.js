754273e86e895dc2450946064562394c096cbf78
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2021-08-31 00:29:36 +0530

    todo init

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,12 +1,34 @@
 
 const express = require('express');
-const https = require('https');
 
 const app = express();
 
 app.use(express.urlencoded());
 
-app.use(express.static('public'));
+var items = [];
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options)
+
+  res.render('list', {kindOfDay: day, newListItems: items});
+
+})
+
+app.post('/', function(req, res){
+  items.push(req.body.newItem);
+  res.redirect('/');
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('server is running on port 3000...')
