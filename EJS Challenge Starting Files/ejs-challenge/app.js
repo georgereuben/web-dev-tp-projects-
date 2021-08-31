4757005604beab5bbfa582dcba22edfbf30d873c
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2021-08-31 12:25:32 +0530

    Challenge 6

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -16,19 +16,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', function(req, res){
-  res.render('home', {homeStartingPara: homeStartingContent});
+  res.render('home', {homeStartingContent});
 })
 
+app.get('/about', function(req, res){
+  res.render('about', {aboutContent});
+})
 
+app.get('/contact', function(req, res){
+  res.render('contact', {contactContent});
+})
 
-
-
-
-
-
-
-
-
+app.get('/compose', function(req, res){
+  res.render('compose')
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
