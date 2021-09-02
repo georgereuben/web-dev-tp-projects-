3fab39a0a55905bd06cf9039f96df41cb4522702
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2021-09-03 00:02:37 +0530

    fin

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
 
 const express = require("express");
-
+const _ = require("lodash")
 const ejs = require("ejs");
+const { forEach } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,8 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', function(req, res){
-  console.log(posts);
-  res.render('home', {homeStartingContent});
+  res.render('home', {homeStartingContent, posts});
 })
 
 app.get('/about', function(req, res){
@@ -32,6 +32,18 @@ app.get('/contact', function(req, res){
 
 app.get('/compose', function(req, res){
   res.render('compose');
+})
+
+app.get('/posts/:postTitle', function(req, res){
+  
+  let post = {};
+  posts.forEach(function(post_){
+    if(_.lowerCase(post_.title) == _.lowerCase(req.params.postTitle)){
+      post = post_;
+      console.log(post);
+    }
+  })
+  res.render('post', {post});
 })
 
 app.post('/compose', function(req, res){
