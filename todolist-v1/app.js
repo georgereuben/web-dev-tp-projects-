1ec0573f4a3103e6076eeaf7269d5823d51c0b9c
Reuben George <69674713+ReubenGeorge02@users.noreply.github.com>
2021-09-04 09:12:34 +0530

    mongoose

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const date = require(__dirname + '/date.js');
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.urlencoded());
@@ -8,12 +9,41 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-const items = [];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+  name: {
+    type: String,
+    unique: true
+  }
+};
+
+const Item = mongoose.model("items", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your ToDo List!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add an item"
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("Default items saved!");
+  }
+});
 
 app.get('/', function(req,res){
-  let day = date.getDate();
-  res.render('list', {listTitle: day, newListItems: items});
+  res.render('list', {listTitle: "Today", newListItems: items});
 })
 
 app.post('/', function(req, res){
